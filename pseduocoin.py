e342a46ebfe3426d63d84dab6e6aa5524414c84a
ZombieNeo <46230458+ZombieNeo@users.noreply.github.com>
2021-04-28 16:04:25 +0100

    BIRTH 1.0

---
+++ pseduocoin.py
@@ -0,0 +1,20 @@
+from random import *
+import time 
+from datetime import datetime
+nonce = randint(1,1000000)
+start = datetime.now()
+attempts = 0
+while True:
+    attempts = attempts + 1
+    guess = randint(1,1000000)
+    print(guess)
+    if guess == nonce:
+        end = datetime.now()
+        print("started @" +str(start)+" finished @ "+str(end))
+        print("WINNER!")
+        print("in "+str(attempts)+" attempts")
+        f = open("log.txt","a")
+        f.write("nonce = "+str(nonce)+ " completed "+str(start)+","+str(end)+"\n")
+        f.close()
+        time.sleep(100000)
+        
