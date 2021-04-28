ccaff020ff1bda6107454d9ebf6addf7aefc5808
ZombieNeo <46230458+ZombieNeo@users.noreply.github.com>
2021-04-28 17:25:00 +0100

    V2 BEST OF 3

--- pseduocoin.py
+++ pseduocoin.py
@@ -4,6 +4,7 @@ from datetime import datetime
 nonce = randint(1,1000000)
 start = datetime.now()
 attempts = 0
+loops = 0
 while True:
     attempts = attempts + 1
     guess = randint(1,1000000)
@@ -16,5 +17,9 @@ while True:
         f = open("log.txt","a")
         f.write("nonce = "+str(nonce)+ " completed "+str(start)+","+str(end)+"\n")
         f.close()
-        time.sleep(100000)
+        attempts = 0
+        loops = loops + 1
+        while loops > 3:
+            time.sleep(100000)
         
+       
